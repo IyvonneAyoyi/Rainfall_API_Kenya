07c458f1403059191c26c2d00286ad0396184fdc
IyvonneAyoyi <iyvonneayoyi@gmail.com>
2025-11-30 00:20:28 +0300

    Automatically assign logged-in user when creating a Location

--- locations/views.py
+++ locations/views.py
@@ -11,3 +11,8 @@ class LocationListCreateView(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         # automatically associate the logged-in user with the location
         serializer.save(user=self.request.user)
+# Retrieve, Update, Delete view for a single Location
+class LocationDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
+    permission_classes = [IsAuthenticated]
