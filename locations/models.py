ecdb5ebfcd9e98dc2c87bc00b463d5fa80402d67
IyvonneAyoyi <iyvonneayoyi@gmail.com>
2025-11-29 21:39:20 +0300

    Add Location model and initial migration for locations app

--- locations/models.py
+++ locations/models.py
@@ -1,15 +1,16 @@
-from django.conf import settings
 from django.db import models
+from django.conf import settings
+
 
 class Location(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=150, blank=True)
     latitude = models.FloatField()
     longitude = models.FloatField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        unique_together = ("user", "name")
+        unique_together = ("user", "latitude", "longitude")
 
     def __str__(self):
-        return f"{self.name} ({self.latitude}, {self.longitude})"
+        return self.name or f"{self.latitude}, {self.longitude}"
