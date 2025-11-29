b205e6e98c1a39c4c2916284a55472a941a21133
IyvonneAyoyi <iyvonneayoyi@gmail.com>
2025-11-29 20:25:46 +0300

    Add Location model with user ownership & coordinates

--- locations/models.py
+++ locations/models.py
@@ -1,3 +1,15 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class Location(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("user", "name")
+
+    def __str__(self):
+        return f"{self.name} ({self.latitude}, {self.longitude})"
