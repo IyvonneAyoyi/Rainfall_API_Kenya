6938a879ef6c6a2b492814381058bde2fe420fec
IyvonneAyoyi <iyvonneayoyi@gmail.com>
2025-11-30 00:30:40 +0300

    Update LocationListCreateView to auto-assign logged-in user on creation

--- locations/views.py
+++ locations/views.py
@@ -1,16 +1,20 @@
-from rest_framework import generics
+from rest_framework import generics,permissions
 from rest_framework.permissions import IsAuthenticated
 from .models import Location
 from .serializers import LocationSerializer
 
+# List and Create view for Locations
 class LocationListCreateView(generics.ListCreateAPIView):
     queryset = Location.objects.all()
     serializer_class = LocationSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def perform_create(self, serializer):
+       
         # automatically associate the logged-in user with the location
         serializer.save(user=self.request.user)
+        
+
 # Retrieve, Update, Delete view for a single Location
 class LocationDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Location.objects.all()
