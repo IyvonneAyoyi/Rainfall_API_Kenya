ef329517df90457751880c81455106e014455ccb
IyvonneAyoyi <iyvonneayoyi@gmail.com>
2025-11-30 00:09:53 +0300

    Add LocationListCreateView to list and create locations with user association

--- locations/views.py
+++ locations/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .models import Location
+from .serializers import LocationSerializer
 
-# Create your views here.
+class LocationListCreateView(generics.ListCreateAPIView):
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        # automatically associate the logged-in user with the location
+        serializer.save(user=self.request.user)
