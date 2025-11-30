90c2f5bebed4467f3fa770c6f6f926095ce8143f
IyvonneAyoyi <iyvonneayoyi@gmail.com>
2025-11-30 17:17:40 +0300

    Make latitude and longitude optional to support automatic geocoding

--- locations/serializers.py
+++ locations/serializers.py
@@ -2,6 +2,9 @@ from rest_framework import serializers
 from .models import Location
 
 class LocationSerializer(serializers.ModelSerializer):
+    latitude = serializers.FloatField(required=False)
+    longitude = serializers.FloatField(required=False)
+
     class Meta:
         model = Location
         fields = ['id', 'user', 'name', 'latitude', 'longitude', 'created_at']
