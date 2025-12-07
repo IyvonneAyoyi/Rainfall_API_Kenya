badd91a701175514b33bf322efe1c376f7d06dba
IyvonneAyoyi <iyvonneayoyi@gmail.com>
2025-12-07 15:24:26 +0300

    Create RainfallSerializer for serializing rainfall records

--- locations/serializers.py
+++ locations/serializers.py
@@ -1,18 +1,16 @@
 from rest_framework import serializers
-from django.utils.timezone import localtime
-from .models import Location
+from .models import Rainfall
 
-
-class LocationSerializer(serializers.ModelSerializer):
-    latitude = serializers.FloatField(required=False)
-    longitude = serializers.FloatField(required=False)
-    created_at = serializers.SerializerMethodField()
-
+class RainfallSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Location
-        fields = ['id', 'user', 'name', 'latitude', 'longitude', 'created_at']
-        read_only_fields = ['id', 'created_at', 'user']
-
-    def get_created_at(self, obj):
-        # Convert UTC to local timezone (Africa/Nairobi)
-        return localtime(obj.created_at).strftime("%Y-%m-%d %H:%M:%S")
+        model = Rainfall
+        fields = [
+            'id',
+            'user',
+            'location',
+            'daily_rain_sum',
+            'precipitation_hours',
+            'hourly_rain',
+            'created_at'
+        ]
+        read_only_fields = ['id', 'user', 'created_at']
