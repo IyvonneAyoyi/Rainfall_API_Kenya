eb70c11d91b29b500caf395fb508c151ad7fcdc6
IyvonneAyoyi <iyvonneayoyi@gmail.com>
2025-12-20 23:28:32 +0300

    Register APILog model in Django admin with comprehensive filters

--- api_logs/admin.py
+++ api_logs/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import APILog
 
-# Register your models here.
+@admin.register(APILog)
+class APILogAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'method', 'path', 'status_code', 'ip_address', 'created_at']
+    list_filter = ['method', 'status_code', 'created_at']
+    search_fields = ['path', 'ip_address', 'user__username']
+    readonly_fields = ['created_at', 'user', 'method', 'path', 'status_code', 'ip_address']
