3fac41f9c62d5beed4e21f25dcd991ec70418a1d
IyvonneAyoyi <iyvonneayoyi@gmail.com>
2025-12-20 21:17:15 +0300

    Register Rainfall model in Django admin with list_display and filters

--- rainfall/admin.py
+++ rainfall/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Rainfall
 
-# Register your models here.
+@admin.register(Rainfall)
+class RainfallAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'location', 'created_at']
+    list_filter = ['created_at', 'user', 'location']
+    readonly_fields = ['created_at']
