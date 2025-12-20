243ae9680debd846a5926c387940b47e7b0a30b8
IyvonneAyoyi <iyvonneayoyi@gmail.com>
2025-12-20 21:16:42 +0300

    Register Location model in Django admin with list_display and filters

--- locations/admin.py
+++ locations/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Location
 
-# Register your models here.
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'name', 'latitude', 'longitude', 'created_at']
+    list_filter = ['created_at', 'user']
+    search_fields = ['name']
+    readonly_fields = ['created_at']
