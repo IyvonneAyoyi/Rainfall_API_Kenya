5d39c94587227f9ebba51caf331175094aae04ba
IyvonneAyoyi <iyvonneayoyi@gmail.com>
2025-12-20 22:14:41 +0300

    Add admin-only API log list view

--- api_logs/views.py
+++ api_logs/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import APILog
+from .serializers import APILogSerializer
 
-# Create your views here.
+class APILogListView(generics.ListAPIView):
+    queryset = APILog.objects.all().order_by("-created_at")
+    serializer_class = APILogSerializer
+    permission_classes = [permissions.IsAdminUser]
